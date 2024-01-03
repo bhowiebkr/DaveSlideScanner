e6ca3c47619e9d4a5bb68e8e66eb7a6f59daf227
Bryan <bhowiebkr@gmail.com>
2024-01-02 23:58:11 -0500

    formatting and a bunch of other things

--- Dave_slide_scanner.ino
+++ Dave_slide_scanner.ino
@@ -3,9 +3,10 @@
 #include <LiquidCrystal.h>
 #include "Trigger.h"
 
+// LCD initialization
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
-// define some values used by the panel and buttons
+// Define button values
 int lcd_key = 0;
 int adc_key_in = 0;
 
@@ -14,161 +15,128 @@ int adc_key_in = 0;
 #define btnStep 2
 #define btnNone 3
 
-static const uint8_t projector_pin = 11;  // Output pin used to load the next slide on the projector
-static const uint8_t photo_pin = 12;      // Output pin to take a photo
-static const int slide_load_pin = 13;     // Input pin to know when a slide is loaded
+// Pins for projector, photo, and slide loading
+static const uint8_t projector_pin = 11;
+static const uint8_t photo_pin = 12;
+static const int slide_load_pin = 13;
 
-
-// Timings
+// Timings for various actions
 int trigger_projector_ms = 1;  // Time for the relay to be on to trigger the projector
 int slide_settle_ms = 900;     // Time for the slide to be settled after it's loaded
 int trigger_photo_ms = 500;    // Time to trigger the camera to take the photo
 int wait_photo_ms = 1000;      // Time after the camera took the photo before loading the next slide
 
+
+
+// Trigger instance
 Trigger trigger;
 
+// Debounce for slide loaded input
+static Debounce slideLoadedInput(slide_load_pin, 0, 2);
 
-static Debounce slideLoadedInput(slide_load_pin, 0, 2);  // A Debounce to remove noise in on the input
-
-
+// Enum to represent different states of the scanner
 enum ScannerStates {
-  STOPPED,        // Waiting to start
-  LOADING_SLIDE,  //
+  STOPPED,
+  LOADING_SLIDE,
   WAITING_FOR_SLIDE,
   SLIDE_LOADED,
   TAKING_PHOTO,
   PHOTO_TAKEN,
   STEP
-
 };
 
-ScannerStates scannerState = STOPPED;  // We start the state machine in the STOPPED state
+// Initialize scanner state to STOPPED
+ScannerStates scannerState = STOPPED;
 
+// Function to print to the LCD screen
 void printToLCD(const char* text, int line) {
-  char buf[17];                                  // One extra space for the null terminator
-  snprintf(buf, sizeof(buf), "%-16.16s", text);  // Ensure 16 characters are printed
+  char buf[17];
+  snprintf(buf, sizeof(buf), "%-16.16s", text);
   lcd.setCursor(0, line);
   lcd.print(buf);
 }
 
+// Function to handle the scanner state transitions and actions
 void ScannerMachine() {
-
-
-
-  // Update the machine's parts
   trigger.update();
-
   int btn = get_button();
 
-  if (btn == btnStop) {
-    scannerState = STOPPED;
-  } else if (btn == btnStep) {
-    lcd.print("Status: STEP");
-    delay(400);
-    trigger.trigger(trigger_projector_ms);
-    digitalWrite(projector_pin, HIGH);
-    scannerState = STEP;
-  }
-
-  lcd.setCursor(0, 1);
-  lcd.print(slideLoadedInput.read());
-  lcd.setCursor(0, 0);
-
-
   switch (scannerState) {
-    // Step State to the next slide but don't take a photo (manual mode)
     case STEP:
-      {
-        if (trigger.was_triggered()) {
-          scannerState = STOPPED;
-          digitalWrite(projector_pin, LOW);
-        }
+      // Transition to STOPPED state when trigger was activated
+      if (trigger.was_triggered()) {
+        scannerState = STOPPED;
+        digitalWrite(projector_pin, LOW);
       }
+      break;
 
-    // Stop state. Waiting for the button to start to run
-    // When the button starts, we trigger the projector to load a slide
     case STOPPED:
-      {
-        printToLCD("Status: STOPPED", 0);
-
-        if (btn == btnStart) {
-          delay(400);
-          trigger.trigger(trigger_projector_ms);
-          digitalWrite(projector_pin, HIGH);
-          scannerState = LOADING_SLIDE;
-        }
-        break;
+      // Display STOPPED status and transition to LOADING_SLIDE state when Start button is pressed
+      printToLCD("Status: STOPPED", 0);
+      if (btn == btnStart) {
+        trigger.trigger(trigger_projector_ms);
+        digitalWrite(projector_pin, HIGH);
+        scannerState = LOADING_SLIDE;
       }
+      break;
 
-    // Loading a slide to get ready for a photo state
-    // When the
     case LOADING_SLIDE:
-      {
-
-        printToLCD("Status: LOADING", 0);
-
-        if (trigger.was_triggered()) {
-          digitalWrite(projector_pin, LOW);
-          scannerState = WAITING_FOR_SLIDE;
-        }
-
-        break;
+      // Display LOADING status and transition to WAITING_FOR_SLIDE state when slide loading trigger activated
+      printToLCD("Status: LOADING", 0);
+      if (trigger.was_triggered()) {
+        digitalWrite(projector_pin, LOW);
+        scannerState = WAITING_FOR_SLIDE;
       }
-    // waiting for slide state,
-    // when the slide has been loaded, take the photo
+      break;
+
     case WAITING_FOR_SLIDE:
-      {
-        printToLCD("Status: WAITING", 0);
-
-        if (slideLoadedInput.read()) {
-          scannerState = SLIDE_LOADED;
-          trigger.trigger(slide_settle_ms);
-        }
-        break;
+      // Display WAITING status and transition to SLIDE_LOADED state when slide is detected as loaded
+      printToLCD("Status: WAITING", 0);
+      if (slideLoadedInput.read()) {
+        scannerState = SLIDE_LOADED;
+        trigger.trigger(slide_settle_ms);
       }
+      break;
+
     case SLIDE_LOADED:
-      {
-
-        printToLCD("Status: LOADED", 0);
-
-        if (trigger.was_triggered()) {
-          scannerState = TAKING_PHOTO;
-          trigger.trigger(trigger_photo_ms);
-          digitalWrite(photo_pin, HIGH);
-        }
-        break;
+      // Display LOADED status and transition to TAKING_PHOTO state when trigger activated
+      printToLCD("Status: LOADED", 0);
+      if (trigger.was_triggered()) {
+        scannerState = TAKING_PHOTO;
+        trigger.trigger(trigger_photo_ms);
+        digitalWrite(photo_pin, HIGH);
       }
+      break;
+
     case TAKING_PHOTO:
-      {
-        printToLCD("Status: SHOOT", 0);
-
-
-        if (trigger.was_triggered()) {
-          digitalWrite(photo_pin, LOW);
-          scannerState = PHOTO_TAKEN;
-          trigger.trigger(wait_photo_ms);
-        }
-        break;
+      // Display SHOOT status and transition to PHOTO_TAKEN state when trigger activated
+      printToLCD("Status: SHOOT", 0);
+      if (trigger.was_triggered()) {
+        digitalWrite(photo_pin, LOW);
+        scannerState = PHOTO_TAKEN;
+        trigger.trigger(wait_photo_ms);
       }
+      break;
+
     case PHOTO_TAKEN:
-      {
-        printToLCD("Status: TAKEN", 0);
-
-        if (trigger.was_triggered()) {
-          trigger.trigger(trigger_projector_ms);
-          digitalWrite(projector_pin, HIGH);
-          scannerState = LOADING_SLIDE;
-        }
-        break;
+      // Display TAKEN status and transition to LOADING_SLIDE state when trigger activated
+      printToLCD("Status: TAKEN", 0);
+      if (trigger.was_triggered()) {
+        trigger.trigger(trigger_projector_ms);
+        digitalWrite(projector_pin, HIGH);
+        scannerState = LOADING_SLIDE;
       }
+      break;
   }
 }
 
+// Function to read the button input
 int get_button() {
   adc_key_in = analogRead(0);
 
+  // Analog thresholds to determine the pressed button
   if (adc_key_in > 1000)
-    return btnNone;  // We make this the 1st option for speed reasons since it will be the most likely result
+    return btnNone;
   if (adc_key_in < 50)
     return btnNone;
   if (adc_key_in < 195)
@@ -180,26 +148,24 @@ int get_button() {
   if (adc_key_in < 790)
     return btnStep;
 
-  return btnNone;  // when all others fail, return this...
+  return btnNone;
 }
 
+// Arduino setup function
 void setup() {
-
+  // Initialize pin modes and set them to low
   pinMode(projector_pin, OUTPUT);
   pinMode(photo_pin, OUTPUT);
-
   pinMode(slide_load_pin, INPUT);
-
   digitalWrite(projector_pin, LOW);
   digitalWrite(photo_pin, LOW);
 
-  lcd.begin(16, 2);  // start the LCD
-  lcd.setCursor(0, 0);
-
-  lcd.setCursor(1, 0);
+  // Initialize LCD
+  lcd.begin(16, 2);
   lcd.setCursor(0, 0);
 }
 
+// Arduino main loop
 void loop() {
   lcd.setCursor(0, 0);
   ScannerMachine();
