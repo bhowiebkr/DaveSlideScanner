31d8b4681957c23a994b829c3dbf606743fa847c
Bryan <bhowiebkr@gmail.com>
2024-01-03 01:46:30 -0500

    simplify with using delays instead of balancing triggers.

--- Dave_slide_scanner.ino
+++ Dave_slide_scanner.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "Debounce.h"
 #include <LiquidCrystal.h>
-#include "Trigger.h"
+
 
 // LCD initialization
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
@@ -20,115 +20,29 @@ static const uint8_t projector_pin = 11;
 static const uint8_t photo_pin = 12;
 static const int slide_load_pin = 13;
 
-// Timings for various actions
-int trigger_projector_ms = 1;  // Time for the relay to be on to trigger the projector
-int slide_settle_ms = 900;     // Time for the slide to be settled after it's loaded
-int trigger_photo_ms = 500;    // Time to trigger the camera to take the photo
-int wait_photo_ms = 1000;      // Time after the camera took the photo before loading the next slide
 
-
-
-// Trigger instance
-Trigger trigger;
 
 // Debounce for slide loaded input
-static Debounce slideLoadedInput(slide_load_pin, 0, 2);
+static Debounce slideLoadedInput(slide_load_pin, 0, 20);
 
 // Enum to represent different states of the scanner
 enum ScannerStates {
   STOPPED,
   LOADING_SLIDE,
-  WAITING_FOR_SLIDE,
-  SLIDE_LOADED,
-  TAKING_PHOTO,
-  PHOTO_TAKEN,
-  STEP
+  STEP,
 };
 
 // Initialize scanner state to STOPPED
 ScannerStates scannerState = STOPPED;
 
 // Function to print to the LCD screen
-void printToLCD(const char* text, int line) {
+void printToLCD(const char *text, int line) {
   char buf[17];
   snprintf(buf, sizeof(buf), "%-16.16s", text);
   lcd.setCursor(0, line);
   lcd.print(buf);
 }
 
-// Function to handle the scanner state transitions and actions
-void ScannerMachine() {
-  trigger.update();
-  int btn = get_button();
-
-  switch (scannerState) {
-    case STEP:
-      // Transition to STOPPED state when trigger was activated
-      if (trigger.was_triggered()) {
-        scannerState = STOPPED;
-        digitalWrite(projector_pin, LOW);
-      }
-      break;
-
-    case STOPPED:
-      // Display STOPPED status and transition to LOADING_SLIDE state when Start button is pressed
-      printToLCD("Status: STOPPED", 0);
-      if (btn == btnStart) {
-        trigger.trigger(trigger_projector_ms);
-        digitalWrite(projector_pin, HIGH);
-        scannerState = LOADING_SLIDE;
-      }
-      break;
-
-    case LOADING_SLIDE:
-      // Display LOADING status and transition to WAITING_FOR_SLIDE state when slide loading trigger activated
-      printToLCD("Status: LOADING", 0);
-      if (trigger.was_triggered()) {
-        digitalWrite(projector_pin, LOW);
-        scannerState = WAITING_FOR_SLIDE;
-      }
-      break;
-
-    case WAITING_FOR_SLIDE:
-      // Display WAITING status and transition to SLIDE_LOADED state when slide is detected as loaded
-      printToLCD("Status: WAITING", 0);
-      if (slideLoadedInput.read()) {
-        scannerState = SLIDE_LOADED;
-        trigger.trigger(slide_settle_ms);
-      }
-      break;
-
-    case SLIDE_LOADED:
-      // Display LOADED status and transition to TAKING_PHOTO state when trigger activated
-      printToLCD("Status: LOADED", 0);
-      if (trigger.was_triggered()) {
-        scannerState = TAKING_PHOTO;
-        trigger.trigger(trigger_photo_ms);
-        digitalWrite(photo_pin, HIGH);
-      }
-      break;
-
-    case TAKING_PHOTO:
-      // Display SHOOT status and transition to PHOTO_TAKEN state when trigger activated
-      printToLCD("Status: SHOOT", 0);
-      if (trigger.was_triggered()) {
-        digitalWrite(photo_pin, LOW);
-        scannerState = PHOTO_TAKEN;
-        trigger.trigger(wait_photo_ms);
-      }
-      break;
-
-    case PHOTO_TAKEN:
-      // Display TAKEN status and transition to LOADING_SLIDE state when trigger activated
-      printToLCD("Status: TAKEN", 0);
-      if (trigger.was_triggered()) {
-        trigger.trigger(trigger_projector_ms);
-        digitalWrite(projector_pin, HIGH);
-        scannerState = LOADING_SLIDE;
-      }
-      break;
-  }
-}
 
 // Function to read the button input
 int get_button() {
@@ -167,6 +81,67 @@ void setup() {
 
 // Arduino main loop
 void loop() {
-  lcd.setCursor(0, 0);
-  ScannerMachine();
+  int btn = get_button();
+  bool shouldBreak = false;  // Set the flag to exit the while loop and switch case
+
+  switch (scannerState) {
+    case STOPPED:
+      // Display STOPPED status and transition to LOADING_SLIDE state when Start button is pressed
+      printToLCD("Status: STOPPED", 0);
+      if (btn == btnStart) {
+        scannerState = LOADING_SLIDE;
+      }
+      if (btn == btnStep) {
+        scannerState = STEP;
+      }
+      break;
+    case STEP:
+      printToLCD("Status: STEPPING", 0);
+      digitalWrite(projector_pin, HIGH);
+      delay(5);
+      digitalWrite(projector_pin, LOW);
+      scannerState = STOPPED;
+      delay(200);
+      break;
+
+
+    case LOADING_SLIDE:
+      printToLCD("Status: LOADING", 0);
+      digitalWrite(projector_pin, HIGH);
+      delay(5);
+      digitalWrite(projector_pin, LOW);
+
+      delay(100);  // time so the previous slide get unloaded
+
+      printToLCD("Status: WAITING", 0);
+      int count = 1;
+      while (!slideLoadedInput.read()) {
+        delay(10);
+        char btnText[17];
+        snprintf(btnText, sizeof(btnText), "Time passed: %d", count);
+        printToLCD(btnText, 1);
+        count += 1;
+        if (count > 80) {
+          scannerState = STOPPED;
+          shouldBreak = true;  // Set the flag to exit the while loop and switch case
+          snprintf(btnText, sizeof(btnText), "");
+          printToLCD(btnText, 1);
+          break;
+        }
+      }
+
+      if (!shouldBreak) {
+        delay(200);  // delay so the slide settles
+
+        // Take the photo (delay is how long to have the shutter open to focus)
+        printToLCD("Status: SHOOTING", 0);
+        digitalWrite(photo_pin, HIGH);
+        delay(1000); 
+        digitalWrite(photo_pin, LOW);
+
+        // wait for the photo to be taken
+        printToLCD("Status: CAMERA WAIT", 0);
+        delay(2000);
+      }
+  }
 }
